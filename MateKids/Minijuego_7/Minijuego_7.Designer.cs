4b1230d5eff3ff2461c4e9a497e48e39fb6c69f1
Matt210119 <mattgranja99@gmail.com>
2020-01-13 11:15:15 -0500

    update

--- MateKids/Minijuego_7/Minijuego_7.Designer.cs
+++ MateKids/Minijuego_7/Minijuego_7.Designer.cs
@@ -30,8 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Minijuego7));
             this.iniciar_minijuego = new System.Windows.Forms.Button();
-            this.texto_descripcion = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.texto_descripcion = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,17 +46,6 @@
             this.iniciar_minijuego.UseVisualStyleBackColor = true;
             this.iniciar_minijuego.Click += new System.EventHandler(this.iniciar_minijuego_Click);
             // 
-            // texto_descripcion
-            // 
-            this.texto_descripcion.Font = new System.Drawing.Font("GLYPHICONS Halflings", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.texto_descripcion.ForeColor = System.Drawing.Color.LimeGreen;
-            this.texto_descripcion.Location = new System.Drawing.Point(29, 182);
-            this.texto_descripcion.Name = "texto_descripcion";
-            this.texto_descripcion.Size = new System.Drawing.Size(886, 382);
-            this.texto_descripcion.TabIndex = 4;
-            this.texto_descripcion.Text = resources.GetString("texto_descripcion.Text");
-            this.texto_descripcion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::MateKids.Properties.Resources.divipractis_juego;
@@ -67,14 +56,25 @@
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
+            // texto_descripcion
+            // 
+            this.texto_descripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.texto_descripcion.ForeColor = System.Drawing.Color.LimeGreen;
+            this.texto_descripcion.Location = new System.Drawing.Point(30, 166);
+            this.texto_descripcion.Name = "texto_descripcion";
+            this.texto_descripcion.Size = new System.Drawing.Size(885, 382);
+            this.texto_descripcion.TabIndex = 8;
+            this.texto_descripcion.Text = resources.GetString("texto_descripcion.Text");
+            this.texto_descripcion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Minijuego7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(944, 715);
-            this.Controls.Add(this.iniciar_minijuego);
             this.Controls.Add(this.texto_descripcion);
+            this.Controls.Add(this.iniciar_minijuego);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Minijuego7";
@@ -88,7 +88,7 @@
         #endregion
 
         private System.Windows.Forms.Button iniciar_minijuego;
-        private System.Windows.Forms.Label texto_descripcion;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label texto_descripcion;
     }
 }
